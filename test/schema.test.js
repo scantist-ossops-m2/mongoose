c139a9b810071d7145c110ef55f3df98bc47c3c4
jiongle1 <jiongle@scantist.com>
2024-04-08 23:06:21 +0800

    commit patch 14430029

--- test/schema.test.js
+++ test/schema.test.js
@@ -2662,4 +2662,14 @@ describe('schema', function() {
     });
     assert.equal(schema.path('myMap').$__schemaType.$__schemaType.instance, 'String');
   });
+
+  it('disallows setting special properties with `add()` or constructor (gh-12085)', async function() {
+    const maliciousPayload = '{"__proto__.toString": "Number"}';
+
+    assert.throws(() => {
+      mongoose.Schema(JSON.parse(maliciousPayload));
+    }, /__proto__/);
+
+    assert.ok({}.toString());
+  });
 });
