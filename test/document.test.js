2685d09df056ee0b8f5d16ef3f2f6fe98c30183b
TL-Yao <tongle@scntist.com>
2024-08-07 04:27:44 +0000

    commit patch 15539960

--- test/document.test.js
+++ test/document.test.js
@@ -8695,4 +8695,22 @@ describe('document', function() {
     assert.equal(doc.toObject({ transform: true }).dates[0], '2016');
     //assert.equal(doc.toObject({ transform: true }).arr[0].myDate, '2017');
   });
+
+  it('avoids prototype pollution on init', async function() {
+    const Example = db.model('Example', new Schema({ hello: String }));
+
+    const example = await new Example({ hello: 'world!' }).save();
+    await Example.findByIdAndUpdate(example._id, {
+      $rename: {
+        hello: '__proto__.polluted'
+      }
+    });
+
+    // this is what causes the pollution
+    await Example.find();
+
+    const test = {};
+    assert.strictEqual(test.polluted, undefined);
+    assert.strictEqual(Object.prototype.polluted, undefined);
+  });
 });
