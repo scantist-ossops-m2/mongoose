1dff88b96cd66b90ecbaa485d0684e64ec050d53
TL-Yao <tongle@scntist.com>
2024-08-07 19:30:19 +0000

    commit patch 14430284

--- test/schema.test.js
+++ test/schema.test.js
@@ -2433,4 +2433,14 @@ describe('schema', function() {
 
     assert.ok(customerSchema.nested['card']);
   });
+
+  it('disallows setting special properties with `add()` or constructor (gh-12085)', async function() {
+    const maliciousPayload = '{"__proto__.toString": "Number"}';
+
+    assert.throws(() => {
+      mongoose.Schema(JSON.parse(maliciousPayload));
+    }, /__proto__/);
+
+    assert.ok({}.toString());
+  });
 });
