dbff270092f1dc76e81f2e2ac437ebeab0fb838b
TL-Yao <tongle@scntist.com>
2024-08-09 21:19:03 +0000

    commit patch 15539917

--- test/document.test.js
+++ test/document.test.js
@@ -9325,4 +9325,22 @@ describe('document', function() {
 
     assert.equal(p.nested.test, 'new');
   });
+
+  it('avoids prototype pollution on init', async function() {
+    const Example = db.model('Example', new Schema({ hello: String }));
+
+    const example = await new Example({ hello: 'world!' }).save();
+    await Example.findByIdAndUpdate(example._id, {
+      $rename: {
+        hello: '__proto__.polluted'
+      }
+    });
+
+    // this is what causes the pollution
+    await Example.find();
+
+    const test = {};
+    assert.strictEqual(test.polluted, undefined);
+    assert.strictEqual(Object.prototype.polluted, undefined);
+  });
 });
